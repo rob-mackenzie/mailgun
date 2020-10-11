b80285261467c70bc45184b535af9d93db1b6daf
Rob Mackenzie <rob@5app.com>
2020-10-11 12:01:53 +0100

    add postgress db

--- app.js
+++ app.js
@@ -2,10 +2,30 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
+const {Pool} = require('pg')
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+})
+
 app.get('/', (req, res) => {
     console.log(`req:${req}`)
     res.send('Hello World!')
 })
+.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
 
 app.listen(port, () => {
     console.log(`Example app listening at ${port}`)
